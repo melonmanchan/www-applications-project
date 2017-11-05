8177bebbeb7dbadc07a989ba4ce10455eeeff3d1
Matti Jokitulppo <melonmanchan@gmail.com>
2017-11-05 16:10:58 +0200

    Add initial mongo support

--- server/server.js
+++ server/server.js
@@ -1,9 +1,23 @@
+const client = require('mongodb').MongoClient
+
 const koa = require('koa')
+const bodyParser = require('koa-bodyparser');
+const route = require('koa-route')
 const app = new koa()
 
-app.use(async (ctx) => {
-  ctx.body = 'hi'
-  ctx.response.statusCode = 200
-})
+app.use(bodyParser())
 
-app.listen(3000)
+const url = 'mongodb://localhost:27017/www';
+
+async function setUpApp() {
+  const db = await client.connect(url)
+
+  app.use(route.post('/api', async (ctx) => {
+    ctx.body = 'hi'
+    ctx.response.statusCode = 200
+  }))
+
+  app.listen(3000)
+}
+
+setUpApp()
