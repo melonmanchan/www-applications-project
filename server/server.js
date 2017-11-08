ddc6892caf76d82733c65947ce7ddd2b8ce8dca3
Matti Jokitulppo <melonmanchan@gmail.com>
2017-11-08 16:44:53 +0000

    Add useragent parsing

--- server/server.js
+++ server/server.js
@@ -1,5 +1,7 @@
 const client = require('mongodb').MongoClient
 
+const ua = require('useragent')
+ua(true)
 const koa = require('koa')
 const bodyParser = require('koa-bodyparser');
 const route = require('koa-route')
@@ -13,9 +15,13 @@ async function setUpApp() {
   const db = await client.connect(url)
 
   app.use(route.post('/api', async (ctx) => {
-    ctx.body = 'hi'
+    const agent = ctx.request.header['user-agent'] || ctx.request.header['x-user-agent']
+    const userAgent = ua.parse(agent)
+    ctx.body = userAgent
     ctx.response.statusCode = 200
   }))
+
+  console.log('app listening on port 3000')
 
   app.listen(3000)
 }
