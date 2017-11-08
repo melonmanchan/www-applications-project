1d5e182c31e24b230b2e54bb1a33bebd204e6e5e
Olavi Haapala <ojhaapala@gmail.com>
2017-11-08 18:09:33 +0200

    Add recursive and for loop fibonacci implementations

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 set -e
 set -x
-emcc ./main.cc -o wasm/output.wasm.js -s EXPORTED_FUNCTIONS="['_fibonacci']" -s WASM=1 -O3
-emcc ./main.cc -o asmjs/output.asm.js -s EXPORTED_FUNCTIONS="['_fibonacci']" -O3
+emcc ./main.cc -o wasm/output.wasm.js -s EXPORTED_FUNCTIONS="['_fibonacci', '_fibonacci_for']" -s WASM=1 -O3
+emcc ./main.cc -o asmjs/output.asm.js -s EXPORTED_FUNCTIONS="['_fibonacci', '_fibonacci_for']" -O3
--- main.cc
+++ main.cc
@@ -1,5 +1,25 @@
 extern "C" {
-  int fibonacci (int n) {
-    return n * 2;
+  int fibonacci(int n) {
+    if (n <= 1) {
+      return n;
+    }
+    else {
+      return fibonacci(n - 1) + fibonacci(n - 2);
+    }
+  }
+
+  int fibonacci_for(int n) {
+    int fibo = 1;
+    int fiboPrev = 1;
+    if (n <= 1) {
+      return n;
+    }
+
+    for(int i = 2; i < n; ++i){
+      int temp = fibo;
+      fibo += fiboPrev;
+      fiboPrev = temp;
+    }
+    return fibo;
   }
 }
