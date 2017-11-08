9ed7d318fad1d8dfe394071f063557c2ff15caae
Olavi Haapala <ojhaapala@gmail.com>
2017-11-08 17:40:50 +0200

    Build directly to respective folders

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 set -e
 set -x
-emcc ./main.cc -o build/output.wasm.js -s EXPORTED_FUNCTIONS="['_fibonacci']" -s WASM=1 -O3
-emcc ./main.cc -o build/output.asm.js -s EXPORTED_FUNCTIONS="['_fibonacci']" -O3
+emcc ./main.cc -o wasm/output.wasm.js -s EXPORTED_FUNCTIONS="['_fibonacci']" -s WASM=1 -O3
+emcc ./main.cc -o asmjs/output.asm.js -s EXPORTED_FUNCTIONS="['_fibonacci']" -O3
