0b5335c859d18d3c65a499d56ab011f36acaaa82
Olavi Haapala <ojhaapala@gmail.com>
2017-11-14 00:18:16 +0200

    Run also FF

--- dummy_run.sh
+++ dummy_run.sh
@@ -1,9 +1,15 @@
 for i in $(seq 1 40); do
     for t in wasm asmjs js; do
-        echo "Launching $t with n=$i"
-        chromium-browser http://localhost:8000/docs/$t/?n=$i --incognito
+        url=https://melonmanchan.github.io/www-applications-project/$t/?n=$i
+        echo "Launching url: $url"
+        dur=$(expr $i \* 2)
+        echo "Launching chromium"
+        chromium-browser $url --incognito
+        echo "Going to sleep for $dur seconds"
+        sleep $dur
+        echo "Launching FF"
+        firefox -private-window $url &
         pid=$!
-        dur=$(expr $i \* 3)
         echo "Going to sleep for $dur seconds"
         sleep $dur
     done
