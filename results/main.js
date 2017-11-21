c516d47fa88044f525ed5818d860ca7264712973
m3hrad <mehrad.mohammadi@futurice.com>
2017-11-21 16:45:06 +0200

    Graphs show the average.

--- results/main.js
+++ results/main.js
@@ -1,4 +1,5 @@
 $(function() {
+
   var options = {
     chart: {
       type: 'spline'
@@ -65,6 +66,9 @@ $(function() {
       }
     });
 
+    recursiveData = dataAvg(recursiveData);
+    forLoopData = dataAvg(recursiveData);
+
     options.title = {
       text: 'Results for recursive fibonacci'
     };
@@ -108,4 +112,33 @@ $(function() {
 
 function compare(a, b) {
   return a.data.amount - b.data.amount
+}
+
+function typeAvg(data){
+    const helperArray = data.reduce( (result, item) => {
+        if ( typeof result[item[0]] === 'undefined' ) {
+            result[item[0]] = [item[1]];
+        } else {
+            result[item[0]].push(item[1]);
+        }
+        return result;
+    } ,{});
+
+    let results = [];
+    for(let key in helperArray) {
+        let sum = helperArray[key].reduce(function(a, b) { return a + b; });
+        let avg = sum / helperArray[key].length;
+        results.push([key,avg]);
+    }
+    return results;
+}
+
+function dataAvg(data){
+  let result = {};
+  if (typeof (data.wasm) !== 'undefined') {
+      result.wasm = typeAvg(data.wasm);
+      result.asmjs = typeAvg(data.asmjs);
+      result.js = typeAvg(data.js);
+  }
+  return result;
 }
