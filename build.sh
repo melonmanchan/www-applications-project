be3152bdfad1d373a0e2db06bf9d2951f0d5c90e
Olavi Haapala <ojhaapala@gmail.com>
2017-11-22 17:10:01 +0200

    Fix cc path

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 set -e
 set -x
-emcc ./fibonacci/fibonacci.cc.cc -o fibonacci/wasm/output.wasm.js -s EXPORTED_FUNCTIONS="['_fibonacci', '_fibonacci_for']" -s WASM=1 -O3
-emcc ./fibonacci/fibonacci.cc.cc -o fibonacci/asmjs/output.asm.js -s EXPORTED_FUNCTIONS="['_fibonacci', '_fibonacci_for']" -O3
+emcc ./fibonacci/fibonacci.cc -o fibonacci/wasm/output.wasm.js -s EXPORTED_FUNCTIONS="['_fibonacci', '_fibonacci_for']" -s WASM=1 -O3
+emcc ./fibonacci/fibonacci.cc -o fibonacci/asmjs/output.asm.js -s EXPORTED_FUNCTIONS="['_fibonacci', '_fibonacci_for']" -O3
