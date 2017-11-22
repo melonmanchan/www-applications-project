2d191bddff3294941ceaf1e4a027eae43183cb7f
Matti Jokitulppo <melonmanchan@gmail.com>
2017-11-22 18:27:11 +0200

    Make server api variable

--- server/server.js
+++ server/server.js
@@ -14,60 +14,69 @@ const url = "mongodb://localhost:27017/www";
 
 async function setUpApp() {
   const db = await client.connect(url);
-  const collection = db.collection("results");
 
   app.use(parseUserAgent);
 
   app.use(
-    route.post("/fibonacci", async (ctx, next) => {
-      try {
-        const { type } = ctx.request.body;
+    route.post(
+      "/measurements/:measurementType",
+      async (ctx, measurementType) => {
+        try {
+          const collection = db.collection(`results-${measurementType}`);
+          const { type } = ctx.request.body;
 
-        const data = {
-          ...ctx.request.data,
-          type: type,
-          data: ctx.request.body || {}
-        };
+          const data = {
+            ...ctx.request.data,
+            type: type,
+            data: ctx.request.body || {}
+          };
 
-        console.log(data);
-        await collection.insertOne(data);
-        ctx.response.statusCode = 200;
-        ctx.response.body = data;
-      } catch (e) {
-        console.error(e);
-        ctx.response.statusCode = 500;
-        ctx.response.body = e;
+          console.log(data);
+          await collection.insertOne(data);
+          ctx.response.statusCode = 200;
+          ctx.response.body = data;
+        } catch (e) {
+          console.error(e);
+          ctx.response.statusCode = 500;
+          ctx.response.body = e;
+        }
       }
-    })
+    )
   );
 
   app.use(
-    route.get("/fibonacci", async ctx => {
-      var browser = ctx.request.query.browser || undefined;
-      try {
-        var data;
-        if (typeof browser === "undefined") {
-          data = await collection.find().toArray();
-        } else {
-          switch (browser.toLowerCase()) {
-            case "chrome":
-              browser = "Chrome";
-              break;
-            case "firefox":
-              browser = "Firefox";
-              break;
+    route.get(
+      "/measurements/:measurementType",
+      async (ctx, measurementType) => {
+        var browser = ctx.request.query.browser || undefined;
+        const collection = db.collection(`results-${measurementType}`);
+        try {
+          var data;
+          if (typeof browser === "undefined") {
+            data = await collection.find().toArray();
+          } else {
+            switch (browser.toLowerCase()) {
+              case "chrome":
+                browser = "Chrome";
+                break;
+              case "firefox":
+                browser = "Firefox";
+                break;
+            }
+            data = await collection
+              .find({ "browser.family": browser })
+              .toArray();
           }
-          data = await collection.find({ "browser.family": browser }).toArray();
-        }
 
-        ctx.response.statusCode = 200;
-        ctx.response.body = data;
-      } catch (e) {
-        console.error(e);
-        ctx.response.statusCode = 500;
-        ctx.response.body = e;
+          ctx.response.statusCode = 200;
+          ctx.response.body = data;
+        } catch (e) {
+          console.error(e);
+          ctx.response.statusCode = 500;
+          ctx.response.body = e;
+        }
       }
-    })
+    )
   );
 
   console.log("app listening on port 3000");
