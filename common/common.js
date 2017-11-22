eba8f9c089fabd10cfcece14a2322eb95078789d
Matti Jokitulppo <melonmanchan@gmail.com>
2017-11-22 17:36:25 +0200

    Prettier some stuff, change the index.html

--- common/common.js
+++ common/common.js
@@ -1,56 +1,60 @@
-
 function runAndMeasure(recursiveFibonacci, forLoopFibonacci, type) {
-    var amount = parseInt(window.location.search.split('?n=')[1]);
-    var results = {
-        recursive: 0,
-        forLoop: 0,
-        amount: amount,
-        type: type
+  var amount = parseInt(window.location.search.split("?n=")[1]);
+  var results = {
+    recursive: 0,
+    forLoop: 0,
+    amount: amount,
+    type: type
+  };
+
+  console.log(amount);
+  console.log("Recursive fibonacci starting...");
+  console.time();
+  var rStart = performance.now();
+  for (i = 0; i < amount; i++) {
+    recursiveFibonacci(i);
+  }
+  var rEnd = performance.now();
+  console.timeEnd();
+  console.log("Recursive fibonacci ended...");
+  results.recursive = rEnd - rStart;
+
+  console.log("For loop fibonacci starting...");
+  console.time();
+  var fStart = performance.now();
+  for (i = 0; i < amount; i++) {
+    forLoopFibonacci(i);
+  }
+  var fEnd = performance.now();
+  console.timeEnd();
+  console.log("For loop fibonacci ended...");
+  results.forLoop = fEnd - fStart;
+
+  console.log(JSON.stringify(results));
+
+  // post results to server
+  var url = "https://olavihaapala.fi/api/fibonacci";
+  var request = new XMLHttpRequest();
+  request.onreadystatechange = function() {
+    if (request.readyState == 4) {
+      if (request.status == 200) {
+        data = JSON.parse(request.responseText);
+        console.log("POST success, result:", data);
+        window.title = "done";
+      } else {
+        console.error(
+          "Error in POST! The status is " +
+            request.status +
+            " - " +
+            request.responseText
+        );
+      }
     }
+  };
+  request.open("POST", url, true);
+  request.setRequestHeader("Content-type", "application/json");
+  request.send(JSON.stringify(results));
 
-    console.log(amount);
-    console.log('Recursive fibonacci starting...');
-    console.time();
-    var rStart = performance.now();
-    for (i = 0; i < amount; i++) {
-        recursiveFibonacci(i);
-    }
-    var rEnd = performance.now();
-    console.timeEnd();
-    console.log('Recursive fibonacci ended...');
-    results.recursive = rEnd - rStart;
-
-
-    console.log('For loop fibonacci starting...');
-    console.time();
-    var fStart = performance.now();
-    for (i = 0; i < amount; i++) {
-        forLoopFibonacci(i);
-    }
-    var fEnd = performance.now();
-    console.timeEnd();
-    console.log('For loop fibonacci ended...');
-    results.forLoop = fEnd - fStart;
-
-    console.log(JSON.stringify(results));
-
-    // post results to server
-    var url = 'https://olavihaapala.fi/api/fibonacci';
-    var request = new XMLHttpRequest();
-    request.onreadystatechange = function() {
-        if (request.readyState == 4) {
-            if (request.status == 200) {
-                data = JSON.parse(request.responseText);
-                console.log('POST success, result:', data);
-            } else {
-                console.error('Error in POST! The status is ' + request.status + ' - ' + request.responseText)
-            }
-        }
-    }
-    request.open("POST", url, true);
-    request.setRequestHeader("Content-type", "application/json");
-    request.send(JSON.stringify(results));
-
-    document.querySelector('.recursive').innerHTML = results.recursive;
-    document.querySelector('.for').innerHTML = results.forLoop;
+  document.querySelector(".recursive").innerHTML = results.recursive;
+  document.querySelector(".for").innerHTML = results.forLoop;
 }
--- tests/webdriver.js
+++ tests/webdriver.js
@@ -1,22 +1,55 @@
-const webdriver = require('selenium-webdriver')
-const baseUrl = 'https://melonmanchan.github.io/www-applications-project/'
+const webdriver = require("selenium-webdriver");
+const baseUrl = "https://melonmanchan.github.io/www-applications-project/";
+const projects = [
+  {
+    name: "fibonacci",
+    parameters: ["n=10", "n=20", "n=30"]
+  }
+];
 
 // Input capabilities
 const capabilities = {
-  'browserName' : 'chrome',
-  'browserstack.user' : 'mattijokitulppo2',
-  'browserstack.key' : process.env.STACK_KEY,
-  'browserstack.debug' : 'true',
-  'project': 'www-applications-project',
-  'build' : 'build'
+  "browserstack.user": "mattijokitulppo2",
+  "browserstack.key": process.env.STACK_KEY,
+  "browserstack.debug": "true",
+  project: "www-applications-project",
+  build: "build"
+};
+
+const createBrowserStackCapability = browserName => ({
+  ...capabilities,
+  browserName
+});
+
+const capabilitiesUsed = [
+  createBrowserStackCapability("firefox"),
+  createBrowserStackCapability("chrome"),
+  createBrowserStackCapability("internet explorer"),
+  createBrowserStackCapability("safari"),
+  createBrowserStackCapability("opera"),
+  createBrowserStackCapability("edge"),
+  createBrowserStackCapability("iPad"),
+  createBrowserStackCapability("iPhone"),
+  createBrowserStackCapability("android")
+];
+
+for (const c of capabilitiesUsed) {
+  for (const p of projects) {
+    for (const params of p.parameters) {
+      console.log(
+        `Running test ${p.name} on browser ${
+          c.browserName
+        } with query parameters ${params}`
+      );
+
+      var driver = new webdriver.Builder()
+        .usingServer("http://hub-cloud.browserstack.com/wd/hub")
+        .withCapabilities(c)
+        .build();
+
+      driver.get(`${baseUrl}/${p.name}?${params}`);
+      driver.wait(webdriver.until.titleIs("done"), 20000);
+      driver.quit();
+    }
+  }
 }
-
-var driver = new webdriver.Builder().
-  usingServer('http://hub-cloud.browserstack.com/wd/hub').
-  withCapabilities(capabilities).
-  build()
-
-console.log(capabilities)
-// driver.get('http://www.google.com')
-// driver.wait(webdriver.until.titleIs('done'), 1000)
-// driver.quit()
